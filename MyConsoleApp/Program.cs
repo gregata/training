58676248e4c5fc414a2037e38848b6a5ea598bc0
Gabriel de la Regata <gabriel_de_la_regata@ncci.com>
2019-04-16 12:47:08 -0400

    sorting function added

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -6,7 +6,69 @@ namespace MyConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Regata!");
+            int[] array = new int[] { 10, 14, 8, 2, 21, 34, 8 };
+            array = FirstSortingFunctionFIXED(array);
+
+            PrintResult(array);
         }
+
+        static private void PrintResult(int[] array)
+        {
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.WriteLine(array[j]);
+            }
+
+            // keep the app window open
+            Console.Read();
+        }
+
+        static private int[] FirstSortingFunction(int[] arreglo)
+        {
+            int i = 0;
+            int minimo;
+            int newminimo;
+            minimo = arreglo[0];
+            while (i < arreglo.Length - 1)
+            {
+                minimo = arreglo[0];
+                if (arreglo[i] < minimo)
+                {
+                    newminimo = arreglo[i];
+                    arreglo[i] = arreglo[i - 1];
+                    arreglo[i - 1] = newminimo;
+                    i = 0;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return arreglo;
+        }
+
+        static private int[] FirstSortingFunctionFIXED(int[] arreglo)
+        {
+            int i = 1;
+            int newminimo;
+            while (i < arreglo.Length)
+            {
+                if (arreglo[i] < arreglo[i-1])
+                {
+                    newminimo = arreglo[i];
+                    arreglo[i] = arreglo[i - 1];
+                    arreglo[i - 1] = newminimo;
+                    i = 1;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return arreglo;
+        }
+
     }
 }
